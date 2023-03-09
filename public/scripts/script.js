ac41ff5692fbb17a750eeaec0956dbea475bd0ac
Alexiatech <alexiawiersma@gmail.com>
2023-03-09 15:12:14 +0100

    bericht

---
+++ public/scripts/script.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('form');
+const password1 = document.querySelector('#password');
+const password2 = document.querySelector('#comfirmPassword');
+
+form.addEventListener('submit', (event) => {
+  if (password1.value !== password2.value) {
+    event.preventDefault(); // prevent form submission
+    alert("Passwords don't match. Please try again.");
+  }if (password.value.length < 8) {
+    event.preventDefault(); // prevent form submission
+    alert("Password must be at least 8 characters long. Please try again.");
+  }
+});
+
+
--- server.js
+++ server.js
@@ -1,11 +1,54 @@
 const express = require('express');
 const app = express();
-const chalk = require ('chalk');
+const bodyParser = require('body-parser');
+const port = 1337;
 
+
+app.use("/public", express.static("public"));
+app.set("view engine", "ejs");
 
 app.get ('/', onHome).listen(1337);
 
 function onHome (req, res) {
     res.send ('hallo')
 }
+
+//routing
+
+app.get("/registreren", (req, res) => {
+    res.render("registreren.ejs", {data: port})
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/index', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.get('/header', (req, res) => {
+  res.render('index');
+});
+
+app.get('/form', (req, res) => {
+  res.render('form.ejs');
+});
+
+
+
+
+
+//submitting
+
+app.post('/submit', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password1;
+  const confirmPassword = req.body.password2;
+  const country = req.body.country; 
+  const birthday = req.body.date;
+ 
+
+  console.log(req.body)
+  res.send(`Name: ${name}, Email: ${email}, Password: ${password}, Country: ${country}, Birthday: ${birthday},`);
+});
+
 
