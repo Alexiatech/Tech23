68d94b7ea8a10ba175cdadc868d19a251b3ca8e2
Alexiatech <alexiawiersma@gmail.com>
2023-04-04 22:18:13 +0200

    aangepast

--- public/script/script.js
+++ public/script/script.js
@@ -1,14 +1,29 @@
-const quoteElement = document.getElementById('quote');
+// const quoteElement = document.getElementById('quote');
 
-function updateQuote(){
-  fetch('https://meowfacts.herokuapp.com/')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      quoteElement.innerHTML = data.data;
-    })
-    .catch(error => {
-      console.log(error);
-      quoteElement.innerHTML = "Er is een fout opgetreden bij het laden van de gegevens. Probeer het later opnieuw.";
-    });
+// function updateQuote(){
+//   fetch('https://meowfacts.herokuapp.com/')
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log(data);
+//       quoteElement.innerHTML = data.data;
+//     })
+//     .catch(error => {
+//       console.log(error);
+//       quoteElement.innerHTML = "Er is een fout opgetreden bij het laden van de gegevens. Probeer het later opnieuw.";
+//     });
+// }
+
+
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+  alert("Geolocation is not supported by this browser.");
+}
+
+function showPosition(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  // Doe hier iets met de verkregen locatiegegevens, bijvoorbeeld:
+  alert("Latitude: " + latitude + "\nLongitude: " + longitude);
 }
--- server.js
+++ server.js
@@ -2,23 +2,49 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 2000;
 const passwordCon = process.env.PASSWORD
-const session = require('express-session');
-
-
 const bcrypt = require('bcrypt');
+
+let db = null;
+let randomQuote;
+
+async function connectDB() {
+  console.log('connecting')
+  const uri = "mongodb+srv://alexiawiersma:" + passwordCon + "@alexiawiersma.5d12jf8.mongodb.net/?retryWrites=true&w=majority"
+  const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true, serverApi: ServerApiVersion.v1
+  });
+  try {
+      await client.connect();
+      console.log('connected!')
+      db = client.db('Gebruikersgegevens');
+
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+
+
+
+
+//server configurations
+app.listen(port, async () => {
+  console.log('Server started on port 2000');
+  let databaseConnection = await connectDB();
+  let theData = await db.collection('gegevens').find({}).toArray();
+  console.log(theData);
+});
+
+
 
 app.use("/public", express.static("public"));
 app.set("view engine", "ejs");
 
-app.use(session({
-  secret: 'geheime_sleutel',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
 
 //routing
 
@@ -33,9 +59,32 @@ app.get('/form', (req, res) => {
   res.render('form.ejs');
 });
 
+app.get('/location', (req, res) => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        console.log(`Latitude: ${lat}, Longitude: ${long}`);
+        res.send(`Latitude: ${lat}, Longitude: ${long}`);
+      },
+      error => {
+        console.error(error);
+        res.send('Unable to retrieve location.');
+      }
+    );
+  } else {
+    res.send('Geolocation is not supported by this browser.');
+  }
+});
+
+
 app.get('/login', (req, res) => {
   res.render('inloggen.ejs');
 });
+
+
+
 
 app.post('/submit', async (req, res) => {
   const name = req.body.name;
@@ -64,11 +113,9 @@ app.post('/submit', async (req, res) => {
     };
     await db.collection('gegevens').insertOne(userdata);
 
-    // store the user data in session or temporary storage
-    req.session.userdata = userdata;
 
     // redirect the user to the confirmation page
-    res.redirect('/login');
+    res.redirect('/');
   }
   
 });
@@ -93,78 +140,27 @@ app.post('/login', async (req, res) => {
   if (!isMatch) {
     return res.render('inloggen', { error: 'Invalid email or password' });
   } else {
-      // Store the user's name in the session
-      req.session.username = user.name;
   
-     
+    res.redirect('/');
   }
 
-  req.session.userId = user._id;
-
-  res.redirect('/');
 });
 
 
 
 // gegevens updaten 
 
-app.get('/update', async (req, res) => {
-  const userId = req.session.userId;
+// app.get('/update', async (req, res) => {
+//   const userId = req.session.userId;
 
-  const user = await db.collection('gegevens').findOne({ _id: ObjectId(userId) });
+//   const user = await db.collection('gegevens').findOne({ _id: ObjectId(userId) });
 
-  if (!user) {
-    return res.redirect('/login');
-  }
+//   if (!user) {
+//     return res.redirect('/login');
+//   }
 
-  res.render('update', { user });
-});
-
-app.post('/update', async (req, res) => {
-  const userId = req.session.userId;
-
-  const name = req.body.name;
-  const email = req.body.email;
-  const country = req.body.country; 
-  const birthday = req.body.date;
-
-  await db.collection('gegevens').updateOne(
-    { _id: ObjectId(userId) },
-    { $set: { name, email, country, birthday } }
-  );
-
-  res.redirect('/confirmation');
-});
+//   res.render('update', { user });
+// });
 
 
-
-// mongodb 
-
-let db = null;
-
-async function connectDB() {
-  console.log('connecting')
-  const uri = "mongodb+srv://alexiawiersma:" + passwordCon + "@alexiawiersma.5d12jf8.mongodb.net/?retryWrites=true&w=majority"
-  const client = new MongoClient(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true, serverApi: ServerApiVersion.v1
-  });
-  try {
-      await client.connect();
-      console.log('connected!')
-      db = client.db('Gebruikersgegevens');
-
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-}
-
-//server configurations
-app.listen(port, async () => {
-  console.log('Server started on port 2000');
-  let databaseConnection = await connectDB();
-  let theData = await db.collection('gegevens').find({}).toArray();
-  console.log(theData);
-});
 
